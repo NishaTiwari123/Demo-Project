227116cd0c83aa5d10dd475113c1db9394b2b3ff
NishaTiwari123 <nt200029@gmail.com>
2022-07-25 11:15:55 +0530

    1 file modified

--- src/app/main/login/login.component.ts
+++ src/app/main/login/login.component.ts
@@ -8,17 +8,30 @@ import { HeaderService } from '../../appServices/header.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private _login: HeaderService,
-              private _router: Router) {}
+  constructor(private _login: HeaderService, private _router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // if(sessionStorage.getItem('isLoggedIn')){
+    //   this._router.navigate['./index']
+    // }
+    // else{
+    //   this._router.navigate['./login']
+    // }
+    this.get();
+  }
+  set() {
+    sessionStorage.setItem('isLoggedIn', 'true');
+  }
+  get() {
+    sessionStorage.getItem('isLoggedIn');
+  }
 
-  onLogin(uname,pass) {
-    if(pass.value==123){
+  onLogin(uname, pass) {
+    sessionStorage.setItem('isLoggedIn', 'true');
+    if (pass.value == 123) {
       this._login.loginBtnDetail.next(uname.value);
       this._router.navigate(['/index']);
-    }
-    else{
+    } else {
       alert('Oops... Your password is incorrect');
     }
   }
